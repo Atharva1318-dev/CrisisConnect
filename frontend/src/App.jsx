848d6e55246616cbd3b672da6d90cf118e60676d
Atharva Jadhav <atharvajofficial@gmail.com>
2026-01-31 13:10:56 +0530

    Atharva changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,14 +18,11 @@ import { useNavigate } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import NewsSummarizer from "./components/NewsSummarizer.jsx";
 import AnalyticsPage from "./pages/AnalyticsPage.jsx";
-<<<<<<< HEAD
-import CoordinatorManage from "./pages/CoordinatorManage.jsx";
-
-=======
 import Maps from "./components/Maps.jsx";
 import ContactUs from "./components/ContactUs.jsx";
 import CitizenOwn from "./pages/CitizenOwn.jsx";
->>>>>>> 0b977cb67d79c97a8b4889e36331b54cb02effde
+
+import CoordinatorManage from './pages/CoordinatorManage.jsx';
 const App = () => {
   const userData = useSelector((state) => state.user.userData);
   const navigate = useNavigate();
@@ -57,38 +54,36 @@ const App = () => {
           element={userData ? <Navigate to="/" /> : <Login />}
         />
         <Route path="/crises" element={<NewsSummarizer />} />
-<<<<<<< HEAD
-
-=======
         <Route path="/contact" element={<ContactUs />} />
->>>>>>> 0b977cb67d79c97a8b4889e36331b54cb02effde
-        {userData && userData.role === "citizen" && (
-          <>
-            <Route path="/citizenhome" element={<Citizen />} />
-            <Route path="/sos" element={<Sos />} />
-            <Route path="/imagetext" element={<ImageTextInput />} />
-<<<<<<< HEAD
-=======
-            <Route path="/map" element={<Maps />} />
-            <Route path="/citizenown" element={<CitizenOwn />} />
->>>>>>> 0b977cb67d79c97a8b4889e36331b54cb02effde
-          </>
-        )}
-        {userData && userData.role === "agency" && (
-          <>
-            <Route path="/agencyhome" element={<Agency />} />
-            <Route path="/agency/resources" element={<AnalyticsPage />} />
-          </>
-        )}
-        {userData && userData.role === "coordinator" && (
-          <>
-            <Route path="/coordinatorhome" element={<Coordinator />} />
-            <Route path="/coordinator/manage" element={<CoordinatorManage />} />
-          </>
-        )}
-      </Routes>
+        {
+          userData && userData.role === "citizen" && (
+            <>
+              <Route path="/citizenhome" element={<Citizen />} />
+              <Route path="/sos" element={<Sos />} />
+              <Route path="/imagetext" element={<ImageTextInput />} />
+              <Route path="/map" element={<Maps />} />
+              <Route path="/citizenown" element={<CitizenOwn />} />
+            </>
+          )}
+        {
+          userData && userData.role === "agency" && (
+            <>
+              <Route path="/agencyhome" element={<Agency />} />
+              <Route path="/agency/resources" element={<AnalyticsPage />} />
+            </>
+          )
+        }
+        {
+          userData && userData.role === "coordinator" && (
+            <>
+              <Route path="/coordinatorhome" element={<Coordinator />} />
+              <Route path="/coordinator/manage" element={<CoordinatorManage />} />
+            </>
+          )
+        }
+      </Routes >
       <Footer />
-    </div>
+    </div >
   );
 };
 
